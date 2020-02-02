d87bf2f87176018486511fd401adbef69e574b98
jing1201 <low.jing@hotmail.com>
2020-02-01 20:53:34 -0500

    Updated long description in the package

---
+++ pySearchErr/README.md
@@ -0,0 +1,10 @@
+# pySearchErr
+Detects Python3 exception and searches for the error on Google automatically
+## Installation
+```
+pip install pySearchErr
+```
+## Usage
+```
+pySearchErr <filename.py>
+```
--- pySearchErr/setup.py
+++ pySearchErr/setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pySearchErr',
-      version='0.1',
+      version='0.1.3',
       description='Detects Python3 exception and searches for the error on Google automatically',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/JING1201/SwampHacks2020',
       author='jlionwg',
       license='MIT',
